d8e9be2d995d02ff1d877f580f9fdc9398435493
fabien219200 <39243216+fabien219200@users.noreply.github.com>
2019-12-04 18:26:27 +0100

    Added French option to the command

--- ordis_modules/Warframe/fandom.js
+++ ordis_modules/Warframe/fandom.js
@@ -1,13 +1,25 @@
 const Discord = require('discord.js')
 const fonctions = require('../../ordis_bot')
 
-module.exports.wiki = function (message) {
-var msg = message.content.split(" ").slice(1).join(" ")
-    msg = fonctions.majuscule(msg)
-    msg2 = msg.split(" ").join("_")
+module.exports.wiki = async function (message) {
+
     let embed = new Discord.RichEmbed()
         .setTitle("**__Wiki__**")
         .setColor('#0101F2')
-        .addField("Lien de la page du wiki pour " + msg + " :", "https://warframe.fandom.com/wiki/" + msg2)
+
+    var msg = message.content.split(" ").slice(1).join(" ")
+    var msgFr = msg.split("-").slice(2).join("").trim()
+    if (!msgFr == "") {
+        msgFr = fonctions.majuscule(msgFr)
+        msgFr2 = msgFr.split(" ").join("_")
+        
+        embed.addField("Lien de la page du wiki pour " + msgFr + " :", "https://translate.google.com/translate?client=tw-ob&depth=1&hl=fr&ie=UTF8&rurl=translate.google.com&sl=en&sp=nmt4&tl=fr&u=https://warframe.fandom.com/wiki/" + msgFr2)
+    } else {
+        msg = fonctions.majuscule(msg)
+        msg2 = msg.split(" ").join("_")
+        
+        embed.addField("Lien de la page du wiki pour " + msg + " :", "https://warframe.fandom.com/wiki/" + msg2)
+    }
+    
     message.channel.send(embed)
 }
