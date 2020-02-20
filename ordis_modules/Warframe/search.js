2920e02c99f314920d027f850ec731ee43e99092
Fabien Paugois <fabienpaugois@gmail.com>
2020-02-20 23:00:44 +0100

    fixed gara not recognized by /search

--- ordis_modules/Warframe/search.js
+++ ordis_modules/Warframe/search.js
@@ -7,115 +7,112 @@ module.exports.infos = function (message) {
     var query = message.content.split(" ").slice(1).join(" ")
     var category, desc, released, mastery, name, type, image, recepie
     var data
-        axios.get("https://api.warframestat.us/weapons/search/" + query)
-            .then((response) => {
-                if (response.data.length != 0) {
-                    for (var i = 0; i < response.data.length; i++) {
-                        if (query.toLowerCase() === response.data[i].name.toLowerCase()) {
-                            data = response.data[i]
-                            if (data.name.split(" ")[data.name.split(" ").length - 1] == "Prime") {
-                                category = data.category
-                                desc = data.description
-                                released = modifyDate(data.releaseDate)
-                                mastery = data.masteryReq
-                                name = data.name
-                                type = data.type
-                                image = data.wikiaThumbnail
-                                var embed = new Discord.RichEmbed()
-                                    .setTitle("Données de " + name)
-                                    .setDescription("```fix\nEn cas de problème (infos incorrectes), merci de me le faire savoir pour que je puisse modifier le fichier source.\n```")
-                                    .setColor('#0101F2')
-                                    .setURL("https://warframe.fandom.com/wiki/" + fonctions.majuscule(query, "_"))
-                                    .setThumbnail(image)
-                                    .addField("Categorie", category)
-                                    .addField("Type", type)
-                                    .addField("Description", desc)
-                                    .addField("Date de sortie", released)
-                                    .addField("Rang de maîtrise nécessaire", mastery)
-                                message.channel.send(embed)
-                            } else {
-                                category = data.category
-                                desc = data.description
-                                mastery = data.masteryReq
-                                name = data.name
-                                type = data.type
-                                image = data.wikiaThumbnail
-                                recipe = getRecipe(data.components)
-                                var embed = new Discord.RichEmbed()
-                                    .setTitle("Données de " + name)
-                                    .setDescription("```fix\nEn cas de problème (infos incorrectes), merci de me le faire savoir pour que je puisse modifier le fichier source.\n```")
-                                    .setColor('#0101F2')
-                                    .setURL("https://warframe.fandom.com/wiki/" + fonctions.majuscule(query, "_"))
-                                    .setThumbnail(image)
-                                    .addField("Categorie", category)
-                                    .addField("Type", type)
-                                    .addField("Description", desc)
-                                    .addField("Rang de maîtrise nécessaire", mastery)
-                                    .addField("Recette pour crafter : *" + name + "*", recipe)
-                                message.channel.send(embed)
-                            }
+    axios.get("https://api.warframestat.us/weapons/search/" + query)
+        .then((response) => {
+                for (var i = 0; i < response.data.length; i++) {
+                    if (query.toLowerCase() === response.data[i].name.toLowerCase()) {
+                        data = response.data[i]
+                        if (data.name.split(" ")[data.name.split(" ").length - 1] == "Prime") {
+                            category = data.category
+                            desc = data.description
+                            released = modifyDate(data.releaseDate)
+                            mastery = data.masteryReq
+                            name = data.name
+                            type = data.type
+                            image = data.wikiaThumbnail
+                            var embed = new Discord.RichEmbed()
+                                .setTitle("Données de " + name)
+                                .setDescription("```fix\nEn cas de problème (infos incorrectes), merci de me le faire savoir pour que je puisse modifier le fichier source.\n```")
+                                .setColor('#0101F2')
+                                .setURL("https://warframe.fandom.com/wiki/" + fonctions.majuscule(query, "_"))
+                                .setThumbnail(image)
+                                .addField("Categorie", category)
+                                .addField("Type", type)
+                                .addField("Description", desc)
+                                .addField("Date de sortie", released)
+                                .addField("Rang de maîtrise nécessaire", mastery)
+                            message.channel.send(embed)
+                        } else {
+                            category = data.category
+                            desc = data.description
+                            mastery = data.masteryReq
+                            name = data.name
+                            type = data.type
+                            image = data.wikiaThumbnail
+                            recipe = getRecipe(data.components)
+                            var embed = new Discord.RichEmbed()
+                                .setTitle("Données de " + name)
+                                .setDescription("```fix\nEn cas de problème (infos incorrectes), merci de me le faire savoir pour que je puisse modifier le fichier source.\n```")
+                                .setColor('#0101F2')
+                                .setURL("https://warframe.fandom.com/wiki/" + fonctions.majuscule(query, "_"))
+                                .setThumbnail(image)
+                                .addField("Categorie", category)
+                                .addField("Type", type)
+                                .addField("Description", desc)
+                                .addField("Rang de maîtrise nécessaire", mastery)
+                                .addField("Recette pour crafter : *" + name + "*", recipe)
+                            message.channel.send(embed)
                         }
                     }
-                } else {
-                    axios.get("https://api.warframestat.us/warframes/search/" + query)
-                        .then((response2) => {
-                            if (response2.data.length != 0) {
-                                for (var i = 0; i < response2.data.length; i++) {
-                                    if (query.toLowerCase() === response2.data[i].name.toLowerCase()) {
-                                        data = response2.data[i]
-                                        if (data.name.split(" ")[data.name.split(" ").length - 1] == "Prime") {
-                                            category = data.category
-                                            desc = data.description
-                                            released = modifyDate(data.releaseDate)
-                                            mastery = data.masteryReq
-                                            name = data.name
-                                            type = data.type
-                                            image = data.wikiaThumbnail
-                                            var embed = new Discord.RichEmbed()
-                                                .setTitle("Données de " + name)
-                                                .setDescription("```fix\nEn cas de problème (infos incorrectes), merci de me le faire savoir pour que je puisse modifier le fichier source.\n```")
-                                                .setColor('#0101F2')
-                                                .setURL("https://warframe.fandom.com/wiki/" + fonctions.majuscule(query, "_"))
-                                                .setThumbnail(image)
-                                                .addField("Categorie", category)
-                                                .addField("Type", type)
-                                                .addField("Description", desc)
-                                                .addField("Date de sortie", released)
-                                                .addField("Rang de maîtrise nécessaire", mastery)
-                                            message.channel.send(embed)
-                                        } else {
-                                            category = data.category
-                                            desc = data.description
-                                            mastery = data.masteryReq
-                                            name = data.name
-                                            type = data.type
-                                            image = data.wikiaThumbnail
-                                            var embed = new Discord.RichEmbed()
-                                                .setTitle("Données de " + name)
-                                                .setDescription("```fix\nEn cas de problème (infos incorrectes), merci de me le faire savoir pour que je puisse modifier le fichier source.\n```")
-                                                .setColor('#0101F2')
-                                                .setURL("https://warframe.fandom.com/wiki/" + fonctions.majuscule(query, "_"))
-                                                .setThumbnail(image)
-                                                .addField("Categorie", category)
-                                                .addField("Type", type)
-                                                .addField("Description", desc)
-                                                .addField("Rang de maîtrise nécessaire", mastery)
-                                            message.channel.send(embed)
-                                        }
+                }
+                axios.get("https://api.warframestat.us/warframes/search/" + query)
+                    .then((response2) => {
+                        if (response2.data.length != 0) {
+                            for (var i = 0; i < response2.data.length; i++) {
+                                if (query.toLowerCase() === response2.data[i].name.toLowerCase()) {
+                                    data = response2.data[i]
+                                    if (data.name.split(" ")[data.name.split(" ").length - 1] == "Prime") {
+                                        category = data.category
+                                        desc = data.description
+                                        released = modifyDate(data.releaseDate)
+                                        mastery = data.masteryReq
+                                        name = data.name
+                                        type = data.type
+                                        image = data.wikiaThumbnail
+                                        var embed = new Discord.RichEmbed()
+                                            .setTitle("Données de " + name)
+                                            .setDescription("```fix\nEn cas de problème (infos incorrectes), merci de me le faire savoir pour que je puisse modifier le fichier source.\n```")
+                                            .setColor('#0101F2')
+                                            .setURL("https://warframe.fandom.com/wiki/" + fonctions.majuscule(query, "_"))
+                                            .setThumbnail(image)
+                                            .addField("Categorie", category)
+                                            .addField("Type", type)
+                                            .addField("Description", desc)
+                                            .addField("Date de sortie", released)
+                                            .addField("Rang de maîtrise nécessaire", mastery)
+                                        message.channel.send(embed)
+                                    } else {
+                                        category = data.category
+                                        desc = data.description
+                                        mastery = data.masteryReq
+                                        name = data.name
+                                        type = data.type
+                                        image = data.wikiaThumbnail
+                                        var embed = new Discord.RichEmbed()
+                                            .setTitle("Données de " + name)
+                                            .setDescription("```fix\nEn cas de problème (infos incorrectes), merci de me le faire savoir pour que je puisse modifier le fichier source.\n```")
+                                            .setColor('#0101F2')
+                                            .setURL("https://warframe.fandom.com/wiki/" + fonctions.majuscule(query, "_"))
+                                            .setThumbnail(image)
+                                            .addField("Categorie", category)
+                                            .addField("Type", type)
+                                            .addField("Description", desc)
+                                            .addField("Rang de maîtrise nécessaire", mastery)
+                                        message.channel.send(embed)
                                     }
                                 }
-                            } else {
-                                message.channel.send("La recherche n'a donnée aucun résultat !\n")
                             }
-                        }).catch(function (err) {
-                            message.channel.send("La recherche n'a donnée aucun résultat !\n" + err)
-                            console.log(err)
-                        })
-                }
-            }).catch(function (err) {
-                message.channel.send("La recherche n'a donnée aucun résultat !\n" + err)
-                console.log(err)
-            })
+                        } else {
+                            message.channel.send("La recherche n'a donnée aucun résultat !\n")
+                        }
+                    }).catch(function (err) {
+                        message.channel.send("La recherche n'a donnée aucun résultat !\n" + err)
+                        console.log(err)
+                    })
+        }).catch(function (err) {
+            message.channel.send("La recherche n'a donnée aucun résultat !\n" + err)
+            console.log(err)
+        })
 }
 
 
