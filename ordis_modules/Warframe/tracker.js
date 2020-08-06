694e4e13858068d858d1230ae5d532a3d319115e
Fabien Paugois <fabien.paugois@viacesi.fr>
2020-08-06 12:43:07 +0200

    Updated code to discord v12.x

--- ordis_modules/Warframe/tracker.js
+++ ordis_modules/Warframe/tracker.js
@@ -3,30 +3,34 @@ const axios = require('axios')
 
 var tabInvasion = []
 
-module.exports.track = function (item) {
-    var invasionCount = 0
+module.exports = {
+    name: 'Track',
+    description: 'WIP',
+    execute(message) {
+        var invasionCount = 0
 
-    axios.get("https://api.warframestat.us/pc/invasions")
-        .then(response => {
-            var invasion = response.data
-            for (var i = 0; i < invasion.length; i++) {
-                if (!invasion[i].completed) {
-                    if (!invasion[i].vsInfestation) {
-                        if (invasion[i].attackerReward.countedItems[0].type == item) {
-                            //console.log("Detonite Injector")
-                            invasionCount++
-                            console.log(invasion[i])
-                        }
-                    } else {
-                        if (invasion[i].defenderReward.countedItems[0].type == item) {
-                            //console.log("Mutagen Mass")
-                            invasionCount++
+        axios.get("https://api.warframestat.us/pc/invasions")
+            .then(response => {
+                var invasion = response.data
+                for (var i = 0; i < invasion.length; i++) {
+                    if (!invasion[i].completed) {
+                        if (!invasion[i].vsInfestation) {
+                            if (invasion[i].attackerReward.countedItems[0].type == item) {
+                                //console.log("Detonite Injector")
+                                invasionCount++
+                                console.log(invasion[i])
+                            }
+                        } else {
+                            if (invasion[i].defenderReward.countedItems[0].type == item) {
+                                //console.log("Mutagen Mass")
+                                invasionCount++
+                            }
                         }
                     }
                 }
-            }
-            console.log(invasionCount + " " + item)
-        }).catch(err => {
-            console.log("err dans track => " + err.message)
-        })
+                console.log(invasionCount + " " + item)
+            }).catch(err => {
+                console.error("err dans track => " + err.message)
+            })
+    }
 }
