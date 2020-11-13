37da18f22339f8a160d7c7d074f7cc16424fdd0d
Fabien <fabienpaugois@gmail.com>
2020-11-13 12:36:36 +0100

    Removed !sondage embed & changed strings format

--- ordis_modules/Discord/sondage.js
+++ ordis_modules/Discord/sondage.js
@@ -4,30 +4,11 @@ module.exports = {
     name: 'Sondage',
     description: 'Makes a survey',
     async execute(message) {
+        const { channel, author } = message
         try {
-            if (message.content.split(" ")[1].startsWith("-")) {
-                var emote = message.content.split("-")[1].split(" ")
-                console.log(emote)
-                var msg = message.content.split("-").slice(2).join(" ").trim()
-                var emoteDesc = "Merci de réagir avec "
-                for (var i = 0; i < emote.length; i++) {
-                    if (emote[i] == "") {
-                        emote.splice(i, 1)
-                        i--
-                    } else {
-                        emote[i] = emote[i].replace(/<*:\d*>*/g, "")
-                        if (message.guild.emojis.find(emoji => emoji.name.toLowerCase() === emote[i].replace(/:/, "").toLowerCase())) {
-                            console.log(message.guild.emojis.find(emoji => emoji.name.toLowerCase() === emote[i].toLowerCase()))
-                            emoteDesc = emoteDesc + " " + message.guild.emojis.find(emoji => emoji.name.toLowerCase() === emote[i].replace(/:/, "").toLowerCase())
-                        } else {
-                            emoteDesc = emoteDesc + emote[i]
-                        }
-                    }
-                }
-            } else {
-                var msg = message.content.split(" ").slice(1).join(" ").trim()
-                var emoteDesc = "Repondre avec :white_check_mark: ou :x:"
-            }
+            var { msg, emoteDesc, emote } = this.getEmotes(message)
+
+            message.delete()
 
             if (msg == "") {
                 throw "Message vide !"
@@ -36,13 +17,20 @@ module.exports = {
                 throw "Emojis non spécifiés"
             }
 
-            var embed = new Discord.MessageEmbed()
-                .setTitle("Message créé par " + message.author.username)
-                .setDescription("" + msg + "\n\n*" + emoteDesc + "*")
-                .setFooter("PS -> Pour toute suggestion sur la mise en forme ou l'apparence des sondages, veuillez vous referer au salon #proposition-commandes.")
-                .setTimestamp()
-                .setColor("#FF0200")
-            message.channel.send(embed)
+            // var embed = new Discord.MessageEmbed()
+            //     .setTitle("Message créé par " + author.username)
+            //     .setDescription("" + msg + "\n\n*" + emoteDesc + "*")
+            //     .setFooter("PS -> Pour toute suggestion sur la mise en forme ou l'apparence des sondages, veuillez vous referer au salon #proposition-commandes.")
+            //     .setTimestamp()
+            //     .setColor("#FF0200")
+            var survey =
+                `**__Nouveau sondage :__**
+\`\`\`${msg}\`\`\`
+*${emoteDesc}*
+
+PS -> Pour toute suggestion sur la mise en forme ou l'apparence des sondages, veuillez vous referer au salon #proposition-commandes.`
+
+            channel.send(survey)
                 .then(function (message) {
                     reaction(message, emote)
                     //message.pin()
@@ -54,8 +42,34 @@ module.exports = {
             var embed = new Discord.MessageEmbed()
                 .setTitle("Erreur !")
                 .setDescription("Une erreur est survenue. Contactez un developpeur pour corriger ce probleme : " + err)
-            message.channel.send(embed)
+            channel.send(embed)
         }
+    }, getEmotes(message) {
+        if (message.content.split(" ")[1].startsWith("-")) {
+            var emote = message.content.split("-")[1].split(" ")
+            console.log(emote)
+            var msg = message.content.split("-").slice(2).join(" ").trim()
+            var emoteDesc = "Merci de réagir avec "
+            for (var i = 0; i < emote.length; i++) {
+                if (emote[i] == "") {
+                    emote.splice(i, 1)
+                    i--
+                } else {
+                    emote[i] = emote[i].replace(/<*:\d*>*/g, "")
+                    if (message.guild.emojis.find(emoji => emoji.name.toLowerCase() === emote[i].replace(/:/, "").toLowerCase())) {
+                        console.log(message.guild.emojis.find(emoji => emoji.name.toLowerCase() === emote[i].toLowerCase()))
+                        emoteDesc = emoteDesc + " " + message.guild.emojis.find(emoji => emoji.name.toLowerCase() === emote[i].replace(/:/, "").toLowerCase())
+                    } else {
+                        emoteDesc = emoteDesc + emote[i]
+                    }
+                }
+            }
+        } else {
+            var msg = message.content.split(" ").slice(1).join(" ").trim()
+            var emoteDesc = "Repondre avec :white_check_mark: ou :x:"
+        }
+
+        return { msg: msg, emoteDesc: emoteDesc, emote: emote }
     }
 }
 
@@ -65,14 +79,14 @@ async function reaction(message, emote) {
             if (message.guild.emojis.find(emoji => emoji.name.toLowerCase() === emote[i].toLowerCase())) {
                 try {
                     await message.react(message.guild.emojis.find(emoji => emoji.name.toLowerCase() === emote[i].toLowerCase()))
-                } catch{
-                    message.channel.send("Emoji \"" + emote[i] + "\" introuvable !")
+                } catch {
+                    message.channel.send(`Emoji "${emote[i]}" introuvable !`)
                 }
             } else {
                 try {
                     await message.react(emote[i])
                 } catch {
-                    message.channel.send("Emoji \"" + emote[i] + "\" introuvable !")
+                    message.channel.send(`Emoji "${emote[i]}" introuvable !`)
                 }
             }
         }
@@ -81,7 +95,7 @@ async function reaction(message, emote) {
             .then(() => {
                 message.react("❌")
             }).catch(function (err) {
-                message.channel.send("" + err)
+                message.channel.send(`${err}`)
             })
     }
 }
