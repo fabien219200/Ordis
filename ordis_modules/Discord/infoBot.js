813bd6ba63abed125f4298f909ebb2737af72961
Fabien Paugois <fabien.paugois@viacesi.fr>
2021-05-11 13:42:13 +0200

    Updated infoBot for slash commands

--- ordis_modules/Discord/infoBot.js
+++ ordis_modules/Discord/infoBot.js
@@ -3,16 +3,15 @@ const Discord = require('discord.js')
 module.exports = {
     name: 'GuildInfo',
     description: 'Gets guild infos',
-    execute(message) {
+    execute(interaction) {
         let embed = new Discord.MessageEmbed()
             .setTitle("Info du bot")
             .setColor('#0101F2')
             .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Question_mark_alternate.svg/90px-Question_mark_alternate.svg.png")
-            .setAuthor(message.author.tag, message.author.avatarURL)
-            .addField("Nombre de membres sur le discord : ", message.guild.memberCount)
-            .addField("Nombre de rôles : ", message.guild.roles.cache.size + " => " + message.guild.roles.cache.map(roles => "  " + roles.name))
-            .setFooter(message.author.tag + ", vous êtes sur le salon " + message.channel.name)
+            .addField("Nombre de membres sur le discord : ", interaction.guild.memberCount)
+            .addField("Nombre de rôles : " + interaction.guild.roles.cache.size, interaction.guild.roles.cache.map(roles => ":white_small_square:" + roles.name))
+            .setFooter(interaction.member.user.tag + ", vous êtes sur le salon " + interaction.channel.name)
             .setTimestamp()
-        message.channel.send(embed)
+        interaction.editReply(embed)
     },
-};
+}
