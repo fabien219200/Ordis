b41ecba396bbe669c45d6ed3ee3ec5f8562e959d
Fabien Paugois <fabien.paugois@viacesi.fr>
2021-05-12 19:42:12 +0200

    Changed vocal for slash commands

--- ordis_modules/Discord/vocal.js
+++ ordis_modules/Discord/vocal.js
@@ -1,34 +1,29 @@
 module.exports = {
     name: 'Vocal',
     description: 'Sets a new name and/or a limit for a channel',
-    async execute(message) {
-        var user = message.guild.members.cache.find(member => member.user.username.toLowerCase() == message.author.username.toLowerCase())
-        if (user.voice.channel != null) {
-            let vocalChannel = message.guild.channels.cache.find(channel => channel.id == user.voice.channel.id)
-            var args = message.content.replace(/!\w+ +/, "").replace(/ +/, " ").split(" ")
-
-            if (!isNaN(parseInt(args[0].trim()))) {
-                vocalChannel = await vocalChannel.setUserLimit(args[0].trim()).catch(e => {
-                    console.log(e)
-                })
-                if (args[1]) {
-                    vocalChannel = await vocalChannel.setName(args.slice(1).join(" ")).catch(e => {
-                        console.log(e)
-                    })
+    async execute(interaction) {
+        var member = interaction.member
+        if (member.voice.channel != null) {
+            let vocalChannel = interaction.guild.channels.cache.find(channel => channel.id == member.voice.channel.id)
+            if(interaction.options.find(option => option.name == "limite")) {
+                var limit = interaction.options.find(option => option.name == "limite").value || null
+                if(limit > 99) {
+                    interaction.editReply("Veuillez entrer une limite inferieure ou egale a **99**!")
+                    return
                 }
-
-            } else if (true) {
-                vocalChannel = await vocalChannel.setUserLimit(0).catch(e => {
-                    console.log(e)
-                })
-                vocalChannel = await vocalChannel.setName(args.join(" ")).catch(e => {
-                    console.log(e)
-                })
             }
-
+            if(interaction.options.find(option => option.name == "nom")) {
+                var name = interaction.options.find(option => option.name == "nom").value || null
+            }
+            vocalChannel = await vocalChannel.setUserLimit(limit).catch(e => {
+                console.log(e)
+            })
+            vocalChannel = await vocalChannel.setName(name).catch(e => {
+                console.log(e)
+            })
+            interaction.editReply(`Le salon s'appelle desormais ${vocalChannel.name} avec comme limite ${vocalChannel.userLimit}`)
         } else {
-            message.reply("Veuillez utiliser cette commande dans un salon vocal !")
+            interaction.editReply("Veuillez utiliser cette commande dans un salon vocal !")
         }
     }
-
 }
