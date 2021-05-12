ed8414055bdde30e2bf963a2969daedd66ab33c8
Fabien Paugois <fabien.paugois@viacesi.fr>
2021-05-12 19:48:42 +0200

    Changed cetus for slash commands

--- ordis_modules/Warframe/cetus.js
+++ ordis_modules/Warframe/cetus.js
@@ -4,28 +4,27 @@ const axios = require('axios')
 module.exports = {
     name: 'Cetus',
     description: 'Gets Cetus state',
-    execute(message) {
-        console.log("Cetus function")
+    execute(interaction) {
         axios.get('https://api.warframestat.us/pc/cetusCycle')
-        .then((response) => {
-            var chaine = response.data.shortString.split(" ")
-            if (response.data.isDay == true) {
-                let embed = new Discord.MessageEmbed()
-                    .setTitle("Cycle Cetus : **Jour**")
-                    .setColor("#ffe387")
-                    .setThumbnail("https://vignette.wikia.nocookie.net/warframe/images/3/32/OstronSyndicateFlag.png/revision/latest?cb=20171017012540")
-                    .addField("Il fait actuellement jour sur Cetus.", "Temps restant : " + chaine[0] + ".")
-                message.channel.send(embed)
-            } else {
-                let embed = new Discord.MessageEmbed()
-                    .setTitle("Cycle Cetus : **Nuit**")
-                    .setColor("#0c0963")
-                    .setThumbnail("https://vignette.wikia.nocookie.net/warframe/images/3/32/OstronSyndicateFlag.png/revision/latest?cb=20171017012540")
-                    .addField("Il fait actuellement nuit sur Cetus", "Temps restant : " + chaine[0] + ".")
-                message.channel.send(embed)
-            }
-        }).catch(function (err) {
-            console.error("err dans status => " + err.message)
-        })
+            .then((response) => {
+                var chaine = response.data.shortString.split(" ")
+                if (response.data.isDay == true) {
+                    let embed = new Discord.MessageEmbed()
+                        .setTitle("Cycle Cetus : **Jour**")
+                        .setColor("#ffe387")
+                        .setThumbnail("https://vignette.wikia.nocookie.net/warframe/images/3/32/OstronSyndicateFlag.png/revision/latest?cb=20171017012540")
+                        .addField("Il fait actuellement jour sur Cetus.", "Temps restant : " + chaine[0] + ".")
+                    interaction.editReply(embed)
+                } else {
+                    let embed = new Discord.MessageEmbed()
+                        .setTitle("Cycle Cetus : **Nuit**")
+                        .setColor("#0c0963")
+                        .setThumbnail("https://vignette.wikia.nocookie.net/warframe/images/3/32/OstronSyndicateFlag.png/revision/latest?cb=20171017012540")
+                        .addField("Il fait actuellement nuit sur Cetus", "Temps restant : " + chaine[0] + ".")
+                    interaction.editReply(embed)
+                }
+            }).catch(function (err) {
+                interaction.editReply(`Une erreur est survenue pour la raison suivante : \n\`\`\`${err}\`\`\``)
+            })
     }
 }
